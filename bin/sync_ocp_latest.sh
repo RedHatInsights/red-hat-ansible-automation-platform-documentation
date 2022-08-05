8b57bff784da64a61c60dfab2f5adb347d7fe12b
Don Naro <dnaro@redhat.com>
2022-08-05 15:26:07 +0100

    2.2 branch for issue #448

---
+++ bin/sync_ocp_latest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the path to the file that contains the ":OCPLatest:" attribute.
+target=target/downstream/attributes/attributes.adoc
+
+# Retrieve the OCP attributes file from the OpenShift docs repository.
+curl https://raw.githubusercontent.com/openshift/openshift-docs/main/_attributes/ocp-attributes.adoc -o ocp-attributes.adoc
+# Save the value of the "product-version" attribute as a variable.
+ocpversion=$(sed -n -e 's/^:product-version: //p' ocp-attributes.adoc)
+# Replace the value of the "OCPLatest" attribute with the value of the "product-version" attribute.
+sed -i -e "s/^:OCPLatest:.*/:OCPLatest: $ocpversion/" $target
+# Delete the OCP attributes file.
+rm -f ocp-attributes.adoc
