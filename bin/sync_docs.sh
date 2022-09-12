c7b40d00194184c23f13d2b2f8e4a6f823c86338
Don Naro <dnaro@redhat.com>
2022-09-12 15:23:21 +0100

    [2.0-ea] Move aap-common folder (#533)
    move aap common to 2.0-ea

--- bin/sync_docs.sh
+++ bin/sync_docs.sh
@@ -19,9 +19,3 @@ rm -rf $target/release-notes
 # Copy the content of the downstream and release-note folders.
 cp -r $source/downstream $target/downstream
 cp -r $source/release-notes $target/release-notes
-
-# Set up the aap-common content in this branch.
-# Add a symlink to aap-common in each titles directory.
-find $target/downstream/titles/ -mindepth 1 -maxdepth 2 -type d -exec ln -sr target/aap-common {} \;
-# Include aap-common content in each master assembly.
-find $target/downstream/titles/ -name master.adoc -exec sed -i -e 's|//include::aap-common|include::aap-common|g' {} \;
