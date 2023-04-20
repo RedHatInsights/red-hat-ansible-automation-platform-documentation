80a659615b496b2ac92ad06a60e32d065fa4a177
Aine Riordan <44700011+ariordan-redhat@users.noreply.github.com>
2023-04-20 11:54:14 +0100

    AAP-11388 Add sync script for AAP 2.4 (#976)

---
+++ bin/sync_docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######
+# This script synchronizes content to the downstream repository.
+# A Jenkins job configures the source and target repositories and runs this script directly.
+# If you commit changes to this script you should verify the Jenkins job runs successfully.
+######
+
+# Set the path to the source and target directories.
+# The source directory contains the content that you want to synchronize.
+source=source
+# The target directory is the location where you want to synchronize content.
+target=target
+
+# Clean the existing downstream and release-note folders.
+rm -rf $target/downstream
+rm -rf $target/release-notes
+
+# Copy the content of the downstream and release-note folders.
+cp -r $source/downstream $target/downstream
+cp -r $source/release-notes $target/release-notes
